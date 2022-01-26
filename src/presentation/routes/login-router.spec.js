a42a3f414eb37babb2c732bc323ed502c5d1c9b8
thalles <thallesgabriel1307@gmail.com>
2022-01-26 19:29:00 -0300

    ensure LoginRouter returns 400 if no email provider

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -1,5 +1,23 @@
-describe('Login Router', () => {
-  test('', () => {
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
 
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
   })
 })
