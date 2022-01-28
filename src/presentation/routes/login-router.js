3de24c0e18cb1d76b5b6a206d86fd05981eb3aec
thalles <thallesgabriel1307@gmail.com>
2022-01-28 18:10:52 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routes/login-router.js
+++ src/presentation/routes/login-router.js
@@ -26,6 +26,6 @@ module.exports = class LoginRouter {
       return HttpResponse.unauthorizedError()
     }
 
-    return HttpResponse.ok()
+    return HttpResponse.ok({ accessToken })
   }
 }
--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -66,7 +66,7 @@ describe('Login Router', () => {
   })
 
   test('Should return 200 when valid credential are provided', () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
     const httpRequest = {
       body: {
         email: 'valid_email@hotmail.com',
@@ -77,6 +77,7 @@ describe('Login Router', () => {
     const httpResponse = sut.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
   })
 
   test('Should return 500 if no httpRequest is provided', () => {
