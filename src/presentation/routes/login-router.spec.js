9dac3f0ef50f0bcd9f729d159d3b9ab7fdaca8f7
thalles <thallesgabriel1307@gmail.com>
2022-01-28 17:37:47 -0300

    ensure LoginRouter returns 500 if an invalid AuthUseCase provided

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -93,4 +93,32 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toEqual(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
+
+  test('Should return 500 if no AuthUseCase is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@hotmail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if  AuthUseCase has no auth method', () => {
+    const sut = new LoginRouter({})
+    const httpRequest = {
+      body: {
+        email: 'any_email@hotmail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
 })
