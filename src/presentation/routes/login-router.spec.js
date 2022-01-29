4d3f80017c50c55d51c346e685a82660677184bd
thalles <thallesgabriel1307@gmail.com>
2022-01-29 20:45:11 -0300

    chore: ensure LoginRouter returns 500 if no EmailValidator is provided

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -207,4 +207,34 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Should return 500 if no EmailValidator is provided', async () => {
+    const sut = new LoginRouter(makeAuthUseCase)
+    const httpRequest = {
+      body: {
+        email: 'any_email@hotmail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return 500 if EmailValidator has no isEmailValid method', async () => {
+    const sut = new LoginRouter(makeAuthUseCase, {})
+    const httpRequest = {
+      body: {
+        email: 'any_email@hotmail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
 })
