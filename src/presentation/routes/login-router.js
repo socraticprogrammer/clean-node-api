7ac08afa8b86553c35f3ed980aa10f282718611d
thalles <thallesgabriel1307@gmail.com>
2022-02-09 18:22:55 -0300

    fix: import MissingParamError and InvalidParamError

--- src/presentation/routes/login-router.js
+++ src/presentation/routes/login-router.js
@@ -1,6 +1,5 @@
 const HttpResponse = require('../helpers/http-response')
-const MissingParamError = require('../errors/missing-param-error')
-const InvalidParamError = require('../errors/invalid-param-error')
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
 
 module.exports = class LoginRouter {
   constructor (authUseCase, emailValidator) {
