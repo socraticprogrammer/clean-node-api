40810c32da71abf9284862499d737766af6237fe
thalles <thallesgabriel1307@gmail.com>
2022-02-12 15:13:51 -0300

    test(validator): ensure EmailValidator return true if validator returns true

--- src/utils/helpers/email-validator.spec.js
+++ src/utils/helpers/email-validator.spec.js
@@ -1,5 +1,13 @@
-describe('Email validator', () => {
-  test('Should return true if validator return true', () => {
+class EmailValidator {
+  isValid (email) {
+    return true
+  }
+}
 
+describe('Email validator', () => {
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid_email@mail.com')
+    expect(isEmailValid).toBeTruthy()
   })
 })
