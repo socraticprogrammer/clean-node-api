942e2f7925818d5a77c6a8bbd83688beb11e4147
thalles <thallesgabriel1307@gmail.com>
2022-04-13 16:42:25 -0300

    test(helper): ensure EmailValidator throws if no email are provided

--- src/utils/helpers/email-validator.spec.js
+++ src/utils/helpers/email-validator.spec.js
@@ -1,5 +1,6 @@
 const EmailValidator = require('./email-validator')
 const validator = require('validator')
+const { MissingParamError } = require('../errors')
 
 const makeSut = () => {
   return new EmailValidator()
@@ -23,5 +24,10 @@ describe('Email validator', () => {
     const sut = makeSut()
     sut.isValid('any_email@mail.com')
     expect(validator.email).toBe('any_email@mail.com')
+  })
+
+  test('Should throw if no email are provided', () => {
+    const sut = makeSut()
+    expect(sut.isValid).toThrow(new MissingParamError('email'))
   })
 })
