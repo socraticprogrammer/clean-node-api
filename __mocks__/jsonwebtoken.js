d4a3cd73ad9eba2f69e1510430ea24d3f0b72f89
thalles <thallesgabriel1307@gmail.com>
2022-04-14 19:34:04 -0300

    test(helper): ensure TokenGenerator call JWT with correct parans

--- __mocks__/jsonwebtoken.js
+++ __mocks__/jsonwebtoken.js
@@ -1,6 +1,8 @@
 module.exports = {
   token: 'any_token',
+  id: '',
   sign (id, secret) {
+    this.id = id
     return this.token
   }
 }
--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -7,7 +7,7 @@ class TokenGenerator {
 }
 
 const makeSut = () => {
-  return new TokenGenerator()
+  return new TokenGenerator(secret)
 }
 
 describe('Token Generator', () => {
@@ -22,5 +22,11 @@ describe('Token Generator', () => {
     const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
+  })
+
+  test('Should call JWT with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
   })
 })
