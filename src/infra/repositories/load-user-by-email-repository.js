0fbd4c29fdff2faaf444f43eb827f3b94f82904e
thalles <thallesgabriel1307@gmail.com>
2022-04-18 14:13:19 -0300

    refactor(repository): move LoadUserByEmailRepository to its own file

---
+++ src/infra/repositories/load-user-by-email-repository.js
@@ -0,0 +1,14 @@
+module.exports = class LoadUserByEmailRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async load (email) {
+    const user = await this.userModel.findOne({ email }, {
+      projection: {
+        password: 1
+      }
+    })
+    return user
+  }
+}
--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,20 +1,6 @@
 const { MongoClient } = require('mongodb')
+const LoadUserByEmailRepository = require('./load-user-by-email-repository')
 let connection, db
-
-class LoadUserByEmailRepository {
-  constructor (userModel) {
-    this.userModel = userModel
-  }
-
-  async load (email) {
-    const user = await this.userModel.findOne({ email }, {
-      projection: {
-        password: 1
-      }
-    })
-    return user
-  }
-}
 
 const makeSut = () => {
   const userModel = db.collection('users')
