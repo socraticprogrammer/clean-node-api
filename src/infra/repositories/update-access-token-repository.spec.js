13f73cdbb5a18bd6e45d9a73dcb85b443426bfb0
thalles <thallesgabriel1307@gmail.com>
2022-04-30 11:54:46 -0300

    test(repository): ensure UpdateAccessToken throws if no params is provided

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -1,3 +1,4 @@
+const MissingParamError = require('../../utils/errors/missing-param-error')
 const MongoHelper = require('../helpers/mongo-helper')
 let db
 
@@ -7,6 +8,14 @@ class UpdateAccessTokenRepository {
   }
 
   async update (userId, accessToken) {
+    if (!userId) {
+      throw new MissingParamError('userId')
+    }
+
+    if (!accessToken) {
+      throw new MissingParamError('accessToken')
+    }
+
     await this.userModel.updateOne({
       _id: userId
     }, {
@@ -62,5 +71,16 @@ describe('UpdateAccessTokenRepository', () => {
     })
     const promise = sut.update(fakeUser._id, 'valid_token')
     expect(promise).rejects.toThrow()
+  })
+
+  test('Should throw if no params is provided', async () => {
+    const { sut, userModel } = makeSut()
+    const { ops: [fakeUser] } = await userModel.insertOne({
+      name: 'any_name',
+      email: 'valid_email@email.com',
+      password: 'hashed_password'
+    })
+    expect(sut.update()).rejects.toThrow(new MissingParamError('userId'))
+    expect(sut.update(fakeUser._id)).rejects.toThrow(new MissingParamError('accessToken'))
   })
 })
