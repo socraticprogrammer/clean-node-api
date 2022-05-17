9de4515b90968a430162aa136a5bc7a0e09561d0
thalles <thallesgabriel1307@gmail.com>
2022-05-17 19:30:08 -0300

    test(middleware): ensure Cors Middleware enable CORS

---
+++ src/main/middlewares/cors.test.js
@@ -0,0 +1,23 @@
+const supertest = require("supertest");
+const { app, router } = require("../config/app");
+
+describe("CORS Middleware", () => {
+  let server;
+  let request;
+
+  beforeAll(() => {
+    server = app.listen();
+    request = supertest(server);
+  });
+
+  test("Should enable CORS", async () => {
+    router.get("/test_cors", (ctx) => {
+      ctx.body = "";
+    });
+
+    const response = await request.get("/test_cors");
+    expect(response.headers["access-control-allow-origin"]).toBe("*");
+    expect(response.headers["access-control-allow-methods"]).toBe("*");
+    expect(response.headers["access-control-allow-headers"]).toBe("*");
+  });
+});
