16da9ac4cd10eb1c300c06824c02c30c35a10eed
thalles <thallesgabriel1307@gmail.com>
2022-05-18 17:20:41 -0300

    chore: add middleware for json content-type be the standard

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,7 +1,11 @@
 const cors = require("../middlewares/cors");
-const jsonParser = require("../middlewares/jsonParser");
+const jsonParser = require("../middlewares/json-parser");
 
 module.exports = (app) => {
   app.use(cors);
   app.use(jsonParser);
+  app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = "json";
+  });
 };
