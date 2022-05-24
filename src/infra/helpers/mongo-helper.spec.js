5b796fd14d9df7fc3a2626cf65dfc899331d4dee
thalles <thallesgabriel1307@gmail.com>
2022-05-24 11:12:59 -0300

    chore: disconnect mongodb at the end of tests

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -1,13 +1,14 @@
-const MongoHelper = require('./mongo-helper')
+const MongoHelper = require("./mongo-helper");
 
-describe('Mongo Helper', () => {
-  test('Should reconnect when getDb() is invoked and client is disconnected', async () => {
-    const sut = MongoHelper
-    await sut.connect(process.env.MONGO_URL)
-    expect(sut.db).toBeTruthy()
-    await sut.disconnect()
-    expect(sut.db).toBeFalsy()
-    await sut.getDb()
-    expect(sut.db).toBeTruthy()
-  })
-})
+describe("Mongo Helper", () => {
+  test("Should reconnect when getDb() is invoked and client is disconnected", async () => {
+    const sut = MongoHelper;
+    await sut.connect(process.env.MONGO_URL);
+    expect(sut.db).toBeTruthy();
+    await sut.disconnect();
+    expect(sut.db).toBeFalsy();
+    await sut.getDb();
+    expect(sut.db).toBeTruthy();
+    await sut.disconnect();
+  });
+});
