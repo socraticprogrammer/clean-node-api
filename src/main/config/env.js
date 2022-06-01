5394a9d67a5ecdcd8f5cda2a955981df8c7c1a6d
thalles <thallesgabriel1307@gmail.com>
2022-05-31 21:39:32 -0300

    chore(config): add tokenSecret

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5858
+  port: process.env.PORT || 5858,
+  tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
