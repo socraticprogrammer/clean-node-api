104e7ee341eb1d3c5a84bb1f40ab1ddbafbd7681
thalles <thallesgabriel1307@gmail.com>
2022-05-31 21:41:07 -0300

    chore(routes): add login route

--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,3 +1,6 @@
+const { adapt } = require('../adapters/express-router-adapter')
+const LoginRouterComposer = require('../composers/login-router-composer')
+
 module.exports = (router) => {
-  router.post("/login", (ctx) => {});
-};
+  router.post('/login', adapt(LoginRouterComposer.compose()))
+}
