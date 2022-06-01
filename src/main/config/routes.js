738e13ebfd77a0e561625cd1b02019c2ce7e7486
thalles <thallesgabriel1307@gmail.com>
2022-06-01 18:51:04 -0300

    refactor(config): import only routes

--- src/main/config/routes.js
+++ src/main/config/routes.js
@@ -3,7 +3,7 @@ const fg = require('fast-glob')
 
 module.exports = (app) => {
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**.js').forEach((file) =>
+  fg.sync('**/src/main/routes/**routes.js').forEach((file) =>
     require(`../../../${file}`)(router)
   )
 }
