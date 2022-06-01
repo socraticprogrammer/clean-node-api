a43d6ef2a095f30974bc49fc3217c177b4a8eb3f
thalles <thallesgabriel1307@gmail.com>
2022-05-31 21:40:14 -0300

    chore(config): dynamically importing routes

--- src/main/config/routes.js
+++ src/main/config/routes.js
@@ -1,8 +1,9 @@
-const fg = require("fast-glob");
+const router = require('express').Router()
+const fg = require('fast-glob')
 
-module.exports = (router) => {
-  require("../routes/login-routes")(router);
-  fg.sync("**/src/main/routes/**.js").forEach((file) =>
+module.exports = (app) => {
+  app.use('/api', router)
+  fg.sync('**/src/main/routes/**.js').forEach((file) =>
     require(`../../../${file}`)(router)
-  );
-};
+  )
+}
