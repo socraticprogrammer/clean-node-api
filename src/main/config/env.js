3f640ddd413d17ff73f7c11fb17e9cd21a63ae98
thalles <thallesgabriel1307@gmail.com>
2022-06-05 01:29:21 -0300

    fix: update envs

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5858,
+  mongoUrl:
+    process.env.MONGO_URL || 'mongodb://root:1234@db:27017/clean-node-api',
+  port: process.env.PORT || 3000,
   tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
