2ad6560fe9ab901151ba500ae21b28349b8daac0
thalles <thallesgabriel1307@gmail.com>
2022-06-13 22:54:41 -0300

    fix: import app

--- src/main/index.js
+++ src/main/index.js
@@ -3,7 +3,7 @@ const env = require('./config/env')
 
 MongoHelper.connect(env.mongoUrl)
   .then(() => {
-    const { app } = require('./config/app')
+    const app = require('./config/app')
     app.listen(env.port, () =>
       console.log(`Server running at http://localhost:${env.port}`)
     )
