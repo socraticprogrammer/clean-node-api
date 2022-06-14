a5277677e77ddf1d71892ce02cee4d5355b2717f
thalles <thallesgabriel1307@gmail.com>
2022-06-13 22:49:38 -0300

    chore: update default uri of mongodb

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mongoUrl:
-    process.env.MONGO_URL || 'mongodb://root:1234@db:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/cleandb',
   port: process.env.PORT || 3000,
   tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
