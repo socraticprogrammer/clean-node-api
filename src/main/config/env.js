5ff7c8bc34af70977b7f4c7d5e83b95c1eefbbde
thalles <thallesgabriel1307@gmail.com>
2022-07-16 02:18:50 -0300

    chore(env): update mongo url

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,7 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/cleandb',
+  mongoUrl:
+    process.env.MONGO_URL ||
+    "mongodb://cleanuser:clean1234@localhost:27017/cleandb",
   port: process.env.PORT || 3000,
-  tokenSecret: process.env.TOKEN_SECRET || 'secret'
-}
+  tokenSecret: process.env.TOKEN_SECRET || "secret",
+};
